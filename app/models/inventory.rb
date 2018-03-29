a7de5a9917be3dc712bc3f820c87045505b17f73
Marco Minotto <marcoaminotto@hotmail.com>
2018-03-29 14:06:16 -0300

    Validations
    - Added validations like presence, numericality, absence, uniqueless and
      assossiated
    - Added the validations in survivors and inventory models

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,10 @@
 class Inventory < ApplicationRecord
   belongs_to :survivor
+
+  validates :owner, presence: true, uniqueless: true, absence: true, numericality: false
+  validates :name_item, presence: true, uniqueless: true, absence: true, numericality: false
+  validates :quantity, presence: true, numericality: true,
+  validates :type, presence: true, absence: true, numericality: false
+  validates :points, presence: true, numericality: true
+
 end
